782e3cc307a051d5147d926268e4443a8fd17ced
PaulNaval <22-1-00759@vsu.edu.ph>
2025-02-14 14:06:13 +0800

    first commit

--- TryCatchLecture/obj/Debug/net8.0/TryCatchLecture.AssemblyInfo.cs
+++ TryCatchLecture/obj/Debug/net8.0/TryCatchLecture.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TryCatchLecture")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dca9c281a6f3a56064a20066adb707bb44408ef3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+127c5ae2fcb4f4d77a11a4e3570c4b0f7ce92b0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TryCatchLecture")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TryCatchLecture")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
